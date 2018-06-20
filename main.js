7131b794d0e128e51c705ee9f9472b5b1fb201a4
nicolasculerier <nicolas.culerier@laposte.net>
2018-06-20 14:26:22 +0200

    Ajout des fichiers exos javascript

---
+++ main.js
@@ -0,0 +1,7 @@
+function psycho(){
+  document.body.style.backgroundColor = "#CA1F7B";
+  document.body.style.color = "#0000FF";
+  
+}
+
+setTimeout("alert('Hey le site xxxvidsxxx est trop bien. Viens dessus stp please')", 10000);
